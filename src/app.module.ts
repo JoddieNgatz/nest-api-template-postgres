f6f0acc858c85ec6c5ddf4d255699766e11a8cc0
Joseph Mureithi <ngatia.joseph@gmail.com>
2023-08-28 02:44:15 +0300

    working init, user created : <nest g res user>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: '',
+      host: '',
+      port: 5432,
+      password: '',
+      username: '',
+      entities: [],
+      database: '',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
